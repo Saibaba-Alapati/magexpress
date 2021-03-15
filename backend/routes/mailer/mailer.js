256d1bbcb4636eb5a3e8fcf0b178ea549cfa2dc7
Alapati-Saibaba <saibabaalapatiofficial@gmail.com>
2021-03-15 14:05:40 +0530

    separated backend.

--- backend/routes/mailer/mailer.js
+++
@@ -1,6 +0,0 @@
-const  express = require('express');
-const mailerroute = express.Router();
-const mailer = require('../../controllers/mailer/mailer.controller');
-
-mailerroute.post('/send-email',mailer.mailer);
-module.exports = mailerroute;
--- src/Authentication/RegisterAccount.tsx
+++ src/Authentication/RegisterAccount.tsx
@@ -1,5 +1,13 @@
 import CreateAccount from '/Users/saibabaalapati/Desktop/magexpress/src/Authentication/CreateAccount.png';
 
 export default function RegisterAccount():any {
+    return(
+        <div className="registerAccount">
+            <img src={CreateAccount} alt="createaccount"/>
+            <div className="form">
+                
+            </div>
 
+        </div>
+    );
 }
