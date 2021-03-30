ef4d4715810813cb2f12050de624c16ba9d272db
Alapati-Saibaba <saibabaalapatiofficial@gmail.com>
2021-03-30 15:54:47 +0530

    created new files

---
+++ src/tracker/DashBoard.tsx
@@ -0,0 +1,7 @@
+export default function DashBoard() {
+    return (
+    <div className="h-screen" style={{backgroundColor:'#050506'}}>
+        <div className="w-1/6 h-screen" style={{backgroundColor:'#101013'}}></div>
+    </div>
+    )
+}
