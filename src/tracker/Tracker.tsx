0b9fb5175cec1467b8965deadda66d1a7b7d381a
Alapati-Saibaba <saibabaalapatiofficial@gmail.com>
2021-05-10 11:10:49 +0530

    tracker designed

--- src/tracker/Tracker.tsx
+++ src/tracker/Tracker.tsx
@@ -1,10 +1,35 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../styles/tracker.scss'
 import Select from 'react-select';
 import chroma from 'chroma-js';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import LibraryAddCheckIcon from '@material-ui/icons/LibraryAddCheck';
+import LinkIcon from '@material-ui/icons/Link';
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 export default function Tracker() {
     let createdat = "Apr 3 2021,at 2:39PM";
     let updatedat = "Apr 4 2021,at 2:39PM"
+    let projectname = "projectTitle";
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            display: 'flex',
+            '& > *': {
+            margin: theme.spacing(1),
+            },
+        },
+        small: {
+            width: theme.spacing(3),
+            height: theme.spacing(3),
+            color:'white',
+            backgroundColor:'purple'
+        },
+        large: {
+            width: theme.spacing(7),
+            height: theme.spacing(7),
+        },
+    }));
     const employeeUnderProject=[
         { value: 'shyam', label: 'shyam', color: '#00B8D9', isFixed: true },
         { value: 'ravi', label: 'ravi', color: '#0052CC', isDisabled: true },
@@ -78,27 +103,49 @@ export default function Tracker() {
             },
         }),
     };
+    const classes = useStyles();
     return (
         <div className="tracker">
             <div className="bodyOfTracker">
                 <div className="trackerInfo">
-                    <p id="projectTitle">ProjectTitle</p>
+                    <p id="projectTitle">{projectname}</p>
                 </div>
                 <div className="linkers">
-                    <button id="addAttachment"></button>
-                    <button id="subTask"></button>
-                    <button id="linkTask"></button>
+                    <button id="addAttachment" style={{margin:'10px',borderRadius:'10px',backgroundColor:'#121212',color:'white',border:'none',padding:'10px'}}>
+                        <AttachFileIcon/>
+                    </button>
+                    <button id="subTask" style={{margin:'10px',borderRadius:'10px',backgroundColor:'#121212',color:'white',border:'none',padding:'10px'}}>
+                        <LibraryAddCheckIcon/>
+                    </button>
+                    <button id="linkTask" style={{margin:'10px',borderRadius:'10px',backgroundColor:'#121212',color:'white',border:'none',padding:'10px'}}>
+                        <LinkIcon/>
+                    </button>
                 </div>
                 <div className="trackerDescription">
                     <textarea className="mainBodyEditor"/>
                 </div>
-                <div className="comments">
-                    <p>example comments one</p>
-                    <p>example comments two</p>
-                    <p>example comments third</p>
-                </div>
-                <div className="addComment">
-                    <input type="text" className="commentEditor" />
+                <div className="commentWrapper">
+                    <div className="commentBox">
+                        <div className="trackerComments">
+                            <div className="cover">
+                                <div className="comment">
+                                    <Avatar className={classes.small}></Avatar>
+                                    <label style={{padding:'10px',color:'white'}}>User Name</label>
+                                </div>
+                                <button className="moreOptions">
+                                    <MoreHorizIcon/>
+                                </button>
+                            </div>
+                            <textarea className="commentArea" style={{}}/>
+                        </div>
+                        <p>example comments two</p>
+                        <p>example comments third</p>
+                        <p>example comments fourth</p>
+                        <div style={{height:'1px',width:'100%',backgroundColor:'white',opacity:'50%'}}></div>
+                        <div className="addComment">
+                            <textarea className="commentEditor" />
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="infoPanel">
